fca30fb1de30ff3c7c9e2ed7c43f595c105f0932
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2025-01-02 12:58:49 +0530

    name changes

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -16,16 +16,16 @@ const About = () => {
             <div className="col-12 col-lg-6 col-xl-7">
               <div className="row justify-content-xl-center">
                 <div className="col-12 col-xl-11">
-                  <h2 className="mb-3">Who Are We?</h2>
-                  <p className="lead fs-4 text-secondary mb-3">
+                  {/* <h2 className="mb-3">Who Are We?</h2> */}
+                  {/* <p className="lead fs-4 text-secondary mb-3">
                     Piyush is a full stack developer guy who uses the lastest
                     MERN technologies to build websites.
-                  </p>
-                  <p className="mb-5">
+                  </p> */}
+                  {/* <p className="mb-5">
                     He is a dedicated developer having expertise in the MERN
                     stack, Bootstrap, Tailwind, TypeScript, and Firebase, to
                     deliever, high-quality, scalable websites.
-                  </p>
+                  </p> */}
                   <div className="row gy-4 gy-md-0 gx-xxl-5X">
                     <div className="col-12 col-md-6">
                       <div className="d-flex">
@@ -35,7 +35,7 @@ const About = () => {
                             Responsive dynamic websites
                           </h2>
                           <p className="text-secondary mb-0">
-                            He is crafting a digital method that subsists life
+                            We are crafting a digital method that subsists life
                             across all mediums.
                           </p>
                         </div>
@@ -47,8 +47,8 @@ const About = () => {
                         <div>
                           <h2 className="h4 mb-3">Latest technologies</h2>
                           <p className="text-secondary mb-0">
-                            He believes in innovation by merging primary with
-                            elaborate ideas.
+                            We are believes in innovation by merging primary
+                            with elaborate ideas.
                           </p>
                         </div>
                       </div>
@@ -59,7 +59,7 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="container p-4">
+        {/* <div className="container p-4">
           <span>Connect</span>
           <a
             href="https://www.linkedin.com/in/piyush-kumar-jha-a29619239/"
@@ -78,7 +78,7 @@ const About = () => {
           <a href="https://discord.com/" target="_blank">
             <img className="m-2" src={discord} height={30} width={30} alt="" />
           </a>
-        </div>
+        </div> */}
       </section>
     </>
   );
